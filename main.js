54f2087ef8229ddaa2ce5402ba3ad45ce2b4e899
MariDei <d.marianna@icloud.com>
2025-01-13 23:36:56 +0200

    Update script to modal window

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
+const modal = document.getElementById('backdrop');
+const openModalBtn = document.getElementById('open-modal-btn');
+const closeModalBtn = document.getElementById('close-modal-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuBtn = document.querySelector('.mobile-nav-menu');
+const mobileCloseBtn = document.querySelector('.mobile-close-btn');
+
 function openModal() {
-  document.getElementById('modal').style.display = 'block';
+  modal.classList.add('is-open');
 }
 
 function closeModal() {
-  document.getElementById('modal').style.display = 'none';
+  modal.classList.remove('is-open');
 }
 
-document.getElementById('openModalBtn').addEventListener('click', openModal);
+function openMobileMenu() {
+  mobileMenu.classList.add('is-open');
+}
 
-document.getElementById('closeModal').addEventListener('click', closeModal);
+function closeMobileMenu() {
+  mobileMenu.classList.remove('is-open');
+}
+
+openModalBtn.addEventListener('click', openModal);
+
+closeModalBtn.addEventListener('click', closeModal);
+
+mobileMenuBtn.addEventListener('click', openMobileMenu);
+
+mobileCloseBtn.addEventListener('click', closeMobileMenu);
 
 window.addEventListener('click', function (event) {
-  if (event.target === document.getElementById('modal')) {
-    closeModal();
-  }
-});
-
-window.addEventListener('beforeunload', function (event) {
-  if (document.getElementById('modal').style.display === 'block') {
+  if (event.target === modal) {
     closeModal();
   }
 });
