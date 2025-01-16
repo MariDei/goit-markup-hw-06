74b909bcd0b35b7347708f591e09a35dc35b561e
MariDei <d.marianna@icloud.com>
2025-01-17 00:09:49 +0200

    Update scroll-bar button

--- main.js
+++ main.js
@@ -35,17 +35,17 @@ window.addEventListener('click', function (event) {
   }
 });
 
-const btnEl = document.querySelector('.scroll-btn');
+const scrollBtn = document.querySelector('.scroll-btn');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 250) {
-    btnEl.style.display = 'flex';
+    scrollBtn.style.display = 'flex';
   } else {
-    btnEl.style.display = 'none';
+    scrollBtn.style.display = 'none';
   }
 });
 
-btnEl.addEventListener('click', () => {
+scrollBtn.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
